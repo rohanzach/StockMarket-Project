ad9f91cbba35a6db3c1ff23e0a6d44ec8601e7d6
Rohan Zacharia <rohanzach@gmail.com>
2024-08-02 21:59:21 +0000

    Integrated api and basic options logic into modules

--- stockmarket/analyze/views.py
+++ stockmarket/analyze/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .logic import get_stock_data
+from .api_logic import call_api
+from .financial_logic import *
 
 # Create your views here.
-
 def index(request):
-    data = get_stock_data()
-    print(data)
+
+    ticker = 'JPM'
+    price_data, options_call_data, options_put_data = call_api(ticker)
+    price_data = add_columns_stock_data(price_data)
+    options_call_data = add_columns_option_data(options_call_data, price_data, type='call')
+    options_put_data = add_columns_option_data(options_put_data, price_data, type='put')
+
+    print(options_put_data[1])
     return HttpResponse("hello world!")
