03842c8e913869af1a1f0ecb07303fc3b0d37a47
Rohan Zacharia <rohanzach@gmail.com>
2024-08-31 03:15:00 +0000

    Made UI progress

--- FastHTML/index.py
+++ FastHTML/index.py
@@ -8,13 +8,14 @@ app,rt = fast_app(live=True)
 @rt('/')
 def get(): 
     ticker_form = Form(
-        Input(type='text', name='ticker', placeholder='Enter Ticker'),
+        Input(type='search', name='ticker', placeholder='Enter Ticker'),
         Input(type='submit', value='Submit'),
         method='post',
-        action='/get-ticker'
+        action='/get-ticker',
+        role = "search"
     )
-    return Div(
-        H1('Enter Ticker'),
+    return  Titled('Options Income Finder',
+        H3('Enter Ticker'),
         ticker_form
     )
 
@@ -29,20 +30,6 @@ def post(ticker: str):
     except:
         return H1('Error')
     
-    price_data['Daily_Returns'] = (price_data['Close'] / price_data['Close'].shift(1)) - 1
-    daily_std = price_data['Daily_Returns'].std()
-    sigma = daily_std * np.sqrt(252)
-    r = 0.0425
-    s = price_data['Close'].iloc[-1]
-    for i in range(1, len(options_put_data)):
-        # Need to deal with the date minus due to -1 answer when subtracting dates during overlaps (Close date = current date)
-        t = (pd.to_datetime(options_put_data[i]['date'][0]) - pd.to_datetime('today')).days/365
-        options_put_data[i]['BS Price'] = blackScholes(r=r, S=s, K = options_put_data[i]['strike'], T=t, sigma=sigma, type='p')
-        options_put_data[i]['Delta'] = delta_calc(r=r, S=s, K = options_put_data[i]['strike'], T=t, sigma=sigma, type='p')
-        options_put_data[i]['Theta'] = theta_calc(r=r, S=s, K = options_put_data[i]['strike'], T=t, sigma=sigma, type='p')
-        options_put_data[i]['Prob Profit'] = calculate_prob_profit_bs(r=r, S=s, K = options_put_data[i]['strike'], T=t, sigma=sigma, type='p')
-        options_put_data[i]['Expected Option-Income Return'] = (1 - options_put_data[i]['Prob Profit']) * options_put_data[i]['Option Income']
-    
     price = price_data.iloc[-1]['Close']
     filtered_data = options_put_data[6][(options_put_data[6]['strike'] < price)]
     # Calculate the days to expiration
@@ -51,13 +38,40 @@ def post(ticker: str):
     # Calculate the SMA and Bollinger Bands
     sma = price_data['Close'].rolling(window=30).mean()
     std = price_data['Close'].rolling(window=30).std()
-    # upper_band = sma + (2 * std)
     lower_band = sma - (2 * std)
     answer_data = filtered_data[(filtered_data['strike'] < lower_band.iloc[-1])]
     max_row = answer_data.loc[answer_data['Expected Option-Income Return'].idxmax()]
+    
     # Display
-    return Div(
+    return Titled('Interesting Put Options',
         H1(f'Ticker: {ticker}'),
+        # Th('Strike'), Th('Option Income'), Th('BS Price'), Th('Delta'), Th('Theta'), Th('Prob Profit'), Th('Expected Option-Income Return')
+        Details(
+            Summary(answer_data['date'].iloc[0]),
+            Table(
+            Tr(
+            Th('Date'),
+            Th('Strike'), 
+            Th('Option Income'), 
+            Th('BS Price'), 
+            Th('Delta'), 
+            Th('Theta'), 
+            Th('Prob Profit'), 
+            Th('Expected Option-Income Return')
+            ),
+            *[Tr(
+                Td(row['date']),
+                Td(f'${row['strike']}'),
+                Td(f'${round(row["Option Income"], 2)}'),
+                Td(f'${round(row["BS Price"]*100, 6)}'),
+                Td(round(row['Delta'], 4)),
+                Td(round(row['Theta'], 4)),
+                Td(round(row['Prob Profit'], 4)),
+                Td(f'${round(row['Expected Option-Income Return'], 2)}')
+            ) for index, row in answer_data.iterrows()]
+            ),
+            open=True
+        ),
         P(max_row)
     )
 
