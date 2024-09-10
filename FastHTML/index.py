1dbe278151332c5deb93ce4536cf68960cd5cce5
Rohan Zacharia <rohanzach@gmail.com>
2024-09-10 23:40:08 +0000

    MVP Maybe

--- FastHTML/index.py
+++ FastHTML/index.py
@@ -7,7 +7,7 @@ import os
 import json
 
 # Header for the HTML
-hdrs = (picolink, Script(src="https://cdn.tailwindcss.com"),
+hdrs = (Script(src="https://cdn.tailwindcss.com"),
     Link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css"))
 
 
@@ -25,32 +25,37 @@ def create_option_table(data, i, band):
     open_value = True if i == 0 else False
     # print(band)
     # style_value='background-color: #d4edda;' if row['strike'] > band else 'background-color: #f8d7da;'
-    return Details(
-            Summary(data['date'].iloc[0]),
-            Table(cls='striped overflow-auto')(
-            Tr(
-            Th('Date'),
-            Th('Strike'), 
-            Th('Option Income'), 
-            Th('BS Price'), 
-            Th('Delta'), 
-            Th('Theta'), 
-            Th('Prob Profit'), 
-            Th('Expected Option-Income'),
-            ),
-            *[Tr()(
-                Td(row['date']),
-                Td(f'${row['strike']}'),
-                Td(f'${round(row["Option Income"], 2)}'),
-                Td(f'${round(row["BS Price"]*100, 4)}'),
-                Td(round(row['Delta'], 4)),
-                Td(round(row['Theta'], 4)),
-                Td(round(row['Prob Profit'], 4)),
-                Td(f'${round(row['Expected Option-Income Return'], 2)}'),
-            ) for index, row in data.iterrows()]
-            ),
-            open=open_value
+    return Div(cls='collapse collapse-plus')(
+        # cls='collapse collapse-plus'
+        H3(f'Option Date: {data.iloc[0]["date"]}', cls='text-xl font-small text-black collapse-title'),
+        Input(type='radio', name='my-accordion-3', checked=open_value),
+        # ,
+        Div(cls='overflow-x-auto collapse-content')(
+            Table(cls='table table-xs text-black')(
+                Tr(
+                    Th('Strike'),
+                    Th('Option Income'),
+                    Th('BS Price'),
+                    Th('Delta'),
+                    Th('Theta'),
+                    Th('Prob Profit'),
+                    Th('Expected Option-Income'),
+                ),
+                Tbody()(
+                    *[Tr()(
+                        Td(f'${row["strike"]}'),
+                        Td(f'${round(row["Option Income"], 2)}'),
+                        Td(f'${round(row["BS Price"]*100, 4)}'),
+                        Td(round(row['Delta'], 4)),
+                        Td(round(row['Theta'], 4)),
+                        Td(round(row['Prob Profit'], 4)),
+                        Td(f'${round(row["Expected Option-Income Return"], 2)}'),
+                    ) for index, row in data.iterrows()]
+                )
+            )
         )
+    )
+
 
 # Create the app and routes
 app = FastHTML(hdrs=hdrs, before=bware)
@@ -117,38 +122,28 @@ def logout(session):
 # Main route
 @rt('/')
 def get(auth): 
-    # ticker_form = Form(
-    #     Input(type='search', name='ticker', placeholder='Enter Ticker'),
-    #     Input(type='submit', value='Submit'),
-    #     method='post',
-    #     action='/get-ticker',
-    #     role = "search"
-    # )
-    # return  Titled('Options Income Finder',
-    #     H3('Enter Ticker'),
-    #     ticker_form
-    # )
-
     ticker_form = Form(method='post', action='/get-ticker')(
         Div(
             Div(
                 Input(type='text', name='ticker', placeholder='Stock Ticker (e.g., AAPL)',
-                      cls = 'block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6', 
+                      cls = 'bg-gray-50 block w-full rounded-full border-0 px-4 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6', 
                       required='yes'),
-                Input(type='submit', value='Submit', cls='px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:ring ring-indigo-300 disabled:opacity-25 transition ease-in-out duration-150'),
+                Input(type='submit', value='Submit', 
+                      cls='w-full mt-4 rounded-full bg-indigo-600 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'),
                 ),
             cls='rounded-md shadow-sm -space-y-px'
             ),
-        Fieldset(
+        Fieldset(cls = 'mt-4')(
             Legend('Option Type', cls='text-sm font-semibold leading-6 text-gray-900'),
             P('Select which option type you would like to analyze', cls='mt-1 text-sm leading-6 text-gray-600'),
-            Div(cls='mt-6 space-y-6')(Div(
-                    Input(id='put', name='option-type', type='radio', checked='yes', cls='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'),
+            Div(cls='mt-6 space-y-6')(
+                Div(
+                    Input(id='put', name='option_type', value='put', type='radio', checked='yes', cls='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'),
                     Label('Put', cls='ml-3 block text-sm font-medium leading-6 text-gray-900'),
                     cls='flex items-center'
                 ),
                 Div(
-                    Input(id='call', name='option-type', type='radio', cls='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'),
+                    Input(id='call', name='option_type', value='call', type='radio', cls='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'),
                     Label('Call', cls='ml-3 block text-sm font-medium leading-6 text-gray-900'),
                     cls='flex items-center'
                 ))
@@ -164,18 +159,18 @@ def get(auth):
             )
 
 
-    return Div(cls='min-h-screen bg-gray-50 flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8')(
+    return Div(cls='min-h-screen bg-gray-50 flex flex-col items-center py-12 px-4 sm:px-6 lg:px-8')(
         static_text,
         ticker_form
     )
 
 @rt('/get-ticker', methods=['POST'])
-def post(ticker: str, auth):
+def post(option_type: str,ticker: str, auth):
     try:
         ticker = ticker.upper()
         price_data, options_call_data, options_put_data = call_api(ticker)
         price_data = add_columns_stock_data(price_data)
-        # options_call_data = add_columns_option_data(options_call_data, price_data, type='call')
+        options_call_data = add_columns_option_data(options_call_data, price_data, type='call')
         options_put_data = add_columns_option_data(options_put_data, price_data, type='put')
     except Exception as e:
         return P(f'Error: {e}')
@@ -185,24 +180,74 @@ def post(ticker: str, auth):
     sma = price_data['Close'].rolling(window=30).mean()
     std = price_data['Close'].rolling(window=30).std()
     lower_band = sma - (2 * std)
+    upper_band = sma + (2 * std)
 
     ui_tables = []
-    for i in range(len(options_put_data)):
-        data = options_put_data[i][options_put_data[i]['strike'] <= price]
-        # data = data[data['strike'] >= lower_band.iloc[-1]]
-        if data.empty:
-            continue
-        else:
-            ui_table = create_option_table(data, i, lower_band.iloc[-1])
-            ui_tables.append(ui_table)    
-    
-    # Display
-    return Titled('Interesting Put Options',
-        H1(f'Ticker: {ticker}'),
-        P(f'Current Price: ${round(price,2)}'),
-        P(f'SMA: ${round(sma.iloc[-1], 2)}, Bollinger Band: {round(lower_band.iloc[-1], 2)}'),
-        # P(f'Bollinger Band: {lower_band.iloc[-1]}')
-        *ui_tables 
-    )
+    if option_type == 'put':
+        for i in range(len(options_put_data)):
+            data = options_put_data[i][options_put_data[i]['strike'] <= price]
+            if data.empty:
+                continue
+            else:
+                ui_table = create_option_table(data, i, lower_band.iloc[-1])
+                ui_tables.append(ui_table)    
+        
+        # Display
+        return Div(cls='min-h-screen bg-gray-50 flex flex-col items-center py-12 px-4 sm:px-6 lg:px-8')(
+            H1(f'Ticker: {ticker}', cls='mt-6 text-center text-3xl font-extrabold text-black'),
+            P(f'Current Price: ${round(price,2)}', cls='mt-2 text-center text-sm text-black'),
+            P(f'SMA: ${round(sma.iloc[-1], 2)}, Bollinger Band: {round(lower_band.iloc[-1], 2)}', cls='mt-2 text-center text-sm text-black'),
+            *ui_tables 
+        )
+    else:
+        for i in range(len(options_call_data)):
+            data = options_call_data[i][options_call_data[i]['strike'] >= price]
+            if data.empty:
+                continue
+            else:
+                ui_table = create_option_table(data, i, upper_band.iloc[-1])
+                ui_tables.append(ui_table)    
+        
+        # Display
+        return Div(cls='min-h-screen bg-gray-50 flex flex-col items-center py-12 px-4 sm:px-6 lg:px-8')(
+            H1(f'Ticker: {ticker}', cls='mt-6 text-center text-3xl font-extrabold text-black'),
+            P(f'Current Price: ${round(price,2)}', cls='mt-2 text-center text-sm text-black'),
+            P(f'SMA: ${round(sma.iloc[-1], 2)}, Bollinger Band: {round(upper_band.iloc[-1], 2)}', cls='mt-2 text-center text-sm text-black'),
+            *ui_tables 
+        )
+
+@rt('/test')
+def test():
+    return Div(
+    Table(
+        Thead(
+            Tr(
+                Th(),
+                Th('Name'),
+                Th('Job'),
+                Th('company'),
+                Th('location'),
+                Th('Last Login'),
+                Th('Favorite Color')
+            )
+        ),
+        Tbody(
+            Tr(
+                Th('1'),
+                Td('Cy Ganderton'),
+                Td('Quality Control Specialist'),
+                Td('Littel, Schaden and Vandervort'),
+                Td('Canada'),
+                Td('12/16/2020'),
+                Td('Blue')
+            )
+        ),
+        cls='table table-xs'
+    ),
+    cls='overflow-x-auto'
+)
+
+
+
 
 serve()
